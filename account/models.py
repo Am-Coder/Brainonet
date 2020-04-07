a9cff198cafeeac4707dcdc467edfc58828e918b
Am-Coder <mishraaman2210@gmail.com>
2020-04-07 17:58:44 +0530

    New Account can now be created

--- account/models.py
+++ account/models.py
@@ -35,8 +35,8 @@ class MyAccountManager(BaseUserManager):
 
 
 class Account(AbstractBaseUser):
-    first_name = models.CharField(verbose_name="first name", max_length=30, null=True)
-    last_name = models.CharField(verbose_name="last name", max_length=30, null=True)
+    first_name = models.CharField(verbose_name="first name", max_length=30, default='')
+    last_name = models.CharField(verbose_name="last name", max_length=30, default='')
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
     is_admin = models.BooleanField(default=False)
