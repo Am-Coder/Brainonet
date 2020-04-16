d752e5eb736fd69ba1305b5efa1a5a5978ac5a58
Am-Coder <mishraaman2210@gmail.com>
2020-04-16 13:57:58 +0530

    Account Utilities added and Pytest configured

--- account/api/views.py
+++ account/api/views.py
@@ -2,13 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from account.models import Authi, Account, Token
 from django.contrib.auth import logout
-from account.textlocal import sendSMS
-import pyotp
 import json
 from account.api.serializers import AccountSerializer, AuthiSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.utils.translation import ugettext_lazy as _
 import logging
+from account.utils import otp_send, otp_authenticate, login
 
 logger = logging.getLogger(__name__)
 
@@ -18,60 +17,12 @@ class OTPGenerator(APIView):
     permission_classes = []
 
     def post(self, request):
-        data = {}
         num = json.loads(request.body)['number']
 
         if Authi.objects.filter(mobile_number=num).exists():
             Authi.objects.get(mobile_number=num).delete()
-        secretKey = pyotp.random_base32()
-        totp = pyotp.TOTP(secretKey, interval=1000000)
-        otp = totp.now()
-        message = otp + " is the OTP for brainonet account verification on your mobile number. W4Fc9njfi5C"
-        resp = sendSMS('MYmp17Fn+I0-BmN5VgYIil5zKuGObFiBJC5bjnTLZC', num, 'BRONET', message)
-        resp = json.loads(resp)
-        logger.info("Message being sent to :" + num)
-        logger.info(message)
-        logger.info(resp)
-        if resp['status'] == "success":
-            data['response'] = resp['status']
-            data['message'] = resp['message']
-            temporary_auth = Authi()
-            temporary_auth.mobile_number = num
-            temporary_auth.otp = secretKey
-            temporary_auth.save()
-        else:
-            data['response'] = _("response.error")
-            data['error_message'] = _("msg.account.otp.not.send")
-
+        data = otp_send(num)
         return Response(data)
-
-# class OTPGenerator(APIView):
-#     authentication_classes = []
-#     permission_classes = []
-#
-#     def post(self, request):
-#         data = {}
-#         num = request.POST.get('phoneNumber')
-#         if Authi.objects.filter(mobile_number=num).exists():
-#             Authi.objects.get(mobile_number=num).delete()
-#         secretKey = pyotp.random_base32()
-#         totp = pyotp.TOTP(secretKey, interval=1000000)
-#         otp = totp.now()
-#         message = otp + " is the OTP for brainonet account verification on your mobile number. W4Fc9njfi5C"
-#         resp = sendSMS('MYmp17Fn+I0-BmN5VgYIil5zKuGObFiBJC5bjnTLZC', num, 'BRONET', message)
-#         resp = json.loads(resp)
-#         if resp['status'] == "success":
-#             data['response'] = resp['status']
-#             data['message'] = resp['message']
-#             temporary_auth = Authi()
-#             temporary_auth.mobile_number = num
-#             temporary_auth.otp = secretKey
-#             temporary_auth.save()
-#         else:
-#             data['response'] = "Error"
-#             data['error_message'] = "OTP is not send. Resend OTP"
-#
-#         return Response(data)
 
 
 class OTPAuthenticate(APIView):
@@ -79,49 +30,14 @@ class OTPAuthenticate(APIView):
     permission_classes = []
 
     def post(self, request):
-        data = {}
+        # data = {}
         info = AuthiSerializer(data=request.data)
         if info.is_valid(raise_exception=True):
             info = info.data
         otp = info.get('otp')
         mobile_no = info.get('mobile_number')
-        try:
-
-            user_otp = Authi.objects.get(mobile_number=mobile_no)
-            if user_otp:
-                secretKey = user_otp.otp
-                totp = pyotp.TOTP(secretKey, interval=1000000)
-
-                if totp.verify(otp):
-                    user_otp.delete()
-                    try:
-                        user = Account.objects.get(mobile_number=mobile_no)
-                        token = Token.objects.create(user=user)
-                        data['response'] = _("response.success")
-                        data['token'] = token.key
-                        data['username'] = user.first_name + " " + user.last_name
-                        return Response(data)
-                    except Account.DoesNotExist:
-                        user = Account()
-                        user.mobile_number = mobile_no
-                        user.save()
-                        token = Token.objects.create(user=user)
-                        data['response'] = _("response.success")
-                        data['token'] = token.key
-                        return Response(data)
-                    except Token.DoesNotExist:
-                        data['response'] = _("response.error")
-                        data['error_message'] = _("msg.account.token.expired")
-                        return Response(data)
-                else:
-                    data['response'] = _("response.error")
-                    data['error_message'] = _("msg.account.otp.invalid")
-                    return Response(data)
-
-        except Authi.DoesNotExist:
-            data['response'] = _("response.error")
-            data['error_message'] = _("msg.account.otp.expired")
-            return Response(data)
+        data = otp_authenticate(otp, mobile_no)
+        return Response(data)
 
 
 class Login(APIView):
@@ -130,30 +46,14 @@ class Login(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        data = {}
         info = AccountSerializer(data=request.data)
 
         if info.is_valid(raise_exception=True):
             info = info.data
         first_name = info.get('first_name')
         last_name = info.get('last_name')
-        try:
-            user = request.user
-            user.first_name = first_name
-            user.last_name = last_name
-            token = request.auth
-            user.save()
-            data['response'] = _("msg.welcome")
-            data['token'] = token.key
-            return Response(data)
-        except Account.DoesNotExist:
-            data['response'] = _("response.error")
-            data['error_message'] = _("msg.account.not.found")
-            return Response(data)
-        except Token.DoesNotExist:
-            data['response'] = _("response.error")
-            data['error_message'] = _("msg.account.token.expired")
-            return Response(data)
+        data = login(request, first_name, last_name)
+        return Response(data)
 
 
 class Logout(APIView):
---
+++ account/utils.py
@@ -0,0 +1,88 @@
+import pyotp
+from account.textlocal import sendSMS
+import json
+from django.utils.translation import ugettext_lazy as _
+from account.models import Authi, Account, Token
+
+
+def otp_send(num):
+    data = {}
+    secretKey = pyotp.random_base32()
+    totp = pyotp.TOTP(secretKey, interval=1000000)
+    otp = totp.now()
+    message = otp + " is the OTP for brainonet account verification on your mobile number. W4Fc9njfi5C"
+    resp = sendSMS('MYmp17Fn+I0-BmN5VgYIil5zKuGObFiBJC5bjnTLZC', num, 'BRONET', message)
+    resp = json.loads(resp)
+    if resp['status'] == "success":
+        data['response'] = resp['status']
+        data['message'] = resp['message']
+        temporary_auth = Authi()
+        temporary_auth.mobile_number = num
+        temporary_auth.otp = secretKey
+        temporary_auth.save()
+    else:
+        data['response'] = _("response.error")
+        data['error_message'] = _("msg.account.otp.not.send")
+    return data
+
+
+def otp_authenticate(otp, mobile_no):
+    data = {}
+    try:
+
+        user_otp = Authi.objects.get(mobile_number=mobile_no)
+        if user_otp:
+            secretKey = user_otp.otp
+            totp = pyotp.TOTP(secretKey, interval=1000000)
+
+            if totp.verify(otp):
+                user_otp.delete()
+                try:
+                    user = Account.objects.get(mobile_number=mobile_no)
+                    token = Token.objects.create(user=user)
+                    data['response'] = _("response.success")
+                    data['token'] = token.key
+                    data['username'] = user.first_name + " " + user.last_name
+                except Account.DoesNotExist:
+                    user = Account()
+                    user.mobile_number = mobile_no
+                    user.save()
+                    token = Token.objects.create(user=user)
+                    data['response'] = _("response.success")
+                    data['token'] = token.key
+                except Token.DoesNotExist:
+                    data['response'] = _("response.error")
+                    data['error_message'] = _("msg.account.token.expired")
+
+                finally:
+                    return data
+
+            else:
+                data['response'] = _("response.error")
+                data['error_message'] = _("msg.account.otp.invalid")
+                return data
+
+    except Authi.DoesNotExist:
+        data['response'] = _("response.error")
+        data['error_message'] = _("msg.account.otp.expired")
+        return data
+
+
+def login(request,first_name,last_name):
+    data = {}
+    try:
+        user = request.user
+        user.first_name = first_name
+        user.last_name = last_name
+        token = request.auth
+        user.save()
+        data['response'] = _("msg.welcome")
+        data['token'] = token.key
+    except Account.DoesNotExist:
+        data['response'] = _("response.error")
+        data['error_message'] = _("msg.account.not.found")
+    except Token.DoesNotExist:
+        data['response'] = _("response.error")
+        data['error_message'] = _("msg.account.token.expired")
+    finally:
+        return data
