010f33f6a920c2a207614d25adaac366b14d2ac2
Am-Coder <mishraaman2210@gmail.com>
2020-04-18 01:49:06 +0530

    Remove Practice Test

--- account/test_w.py
+++
@@ -1,8 +0,0 @@
-# from django.test import TestCase
-import pytest
-# Create your tests here.
-
-
-@pytest.mark.django_db
-def test_was_published_recently_with_old_question(client):
-    assert 1 == 1
