3baf120c62436aa5152f7126877c7463d35b0c79
Am-Coder <mishraaman2210@gmail.com>
2020-04-22 18:03:00 +0530

    AWS S3 media storage configured

--- mysite/productionsettings.py
+++ mysite/productionsettings.py
@@ -213,10 +213,12 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'brainonet-static'
+# AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
