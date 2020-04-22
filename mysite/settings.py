0ca3f930e5415662bdfc7eb044513c8ef4375bd2
Am-Coder <mishraaman2210@gmail.com>
2020-04-22 12:05:50 +0530

    Added production settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,7 +102,7 @@ LOGGING = {
 }
 
 MIDDLEWARE = [
-    # 'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
