705eab29e4f9e6844d5b2f0639b882991883419b
Am-Coder <mishraaman2210@gmail.com>
2020-04-22 14:12:09 +0530

    Prod Changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -192,7 +192,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    # os.path.join(BASE_DIR, 'media'),
     # os.path.join(BASE_DIR, 'personal/static'),
 
 ]
